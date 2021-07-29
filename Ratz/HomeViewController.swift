5b449c8271cf26a1c66d4a405eceae858755b2ba
schueda <a.schuedamenezes@gmail.com>
2021-07-29 11:33:00 -0300

    fixes quiz and test screen

--- Ratz/HomeViewController.swift
+++ Ratz/HomeViewController.swift
@@ -14,12 +14,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func PresentTest(_ sender: Any) {
+    @IBAction func touchedQuiz(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Quiz", bundle: nil)
+        guard let vc = storyboard.instantiateInitialViewController() else { return }
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+
+    }
+    
+    @IBAction func touchedTest(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Test", bundle: nil)
         guard let vc = storyboard.instantiateInitialViewController() else { return }
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true, completion: nil)
+
     }
     
+    @IBAction func touchedRanking(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Ranking", bundle: nil)
+        guard let vc = storyboard.instantiateInitialViewController() else { return }
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+    }
 }
-
